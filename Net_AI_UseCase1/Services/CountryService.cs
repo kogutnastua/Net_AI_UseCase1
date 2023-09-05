ef62e942d5ca44a9cb59dfcdfd5f1ccc5f64a8e1
Anastasiia Kohut <anastasiia.kohut@aceyus.com>
2023-09-05 07:57:23 +0300

    UC#1 Integration

--- Net_AI_UseCase1/Services/CountryService.cs
+++ Net_AI_UseCase1/Services/CountryService.cs
@@ -33,14 +33,14 @@ namespace Net_AI_UseCase1.Services
             }
         }
 
-        private static IEnumerable<Country> FilterByCountryName(IEnumerable<Country> countries, string countryName)
+        public static IEnumerable<Country> FilterByCountryName(IEnumerable<Country> countries, string countryName)
         {
             return string.IsNullOrEmpty(countryName) ? 
                 countries :
                 countries.Where(x => x.Name.Common.ToLower().Contains(countryName.ToLower()));
         }
 
-        private static IEnumerable<Country> FilterByPopulation(IEnumerable<Country> countries, int population)
+        public static IEnumerable<Country> FilterByPopulation(IEnumerable<Country> countries, int population)
         {
             const int milion = 1000000;
             return population < 0 ?
@@ -48,14 +48,14 @@ namespace Net_AI_UseCase1.Services
                 countries.Where(x => x.Population < population * milion);
         }
 
-        private static IEnumerable<Country> SortByCountryName(IEnumerable<Country> countries, string order = "ascend")
+        public static IEnumerable<Country> SortByCountryName(IEnumerable<Country> countries, string order = "ascend")
         {
             return order == "descend" ? 
                 countries.OrderByDescending(x => x.Name.Common) : 
                 countries.OrderBy(x => x.Name.Common);
         }
 
-        private static IEnumerable<Country> GetFirstNRecords(IEnumerable<Country> countries, int number) 
+        public static IEnumerable<Country> GetFirstNRecords(IEnumerable<Country> countries, int number) 
         {
             return number < 0 ?
                 countries :
---
+++ Net_AI_UseCase1_UnitTests/CountryServiceTests.cs
@@ -0,0 +1,105 @@
+﻿using Net_AI_UseCase1.Models;
+using Net_AI_UseCase1.Services;
+using NUnit.Framework.Internal;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Net_AI_UseCase1_UnitTests
+{
+    public class CountryServiceTests
+    {
+        private readonly IEnumerable<Country> countries = new List<Country>
+        {
+            new()
+            {
+                Name = new() { Common = "Ukraine" },
+                Population = 32000000
+            },
+            new()
+            {
+                Name = new() { Common = "USA" },
+                Population = 156000000
+            },
+            new()
+            {
+                Name = new() {Common = "Poland"},
+                Population = 20000000
+            },
+            new()
+            {
+                Name = new() {Common = "France"},
+                Population = 18000000
+            }
+        };
+
+        [Test]
+        public void FilterByCountryName_Returns_Filtered_Records()
+        {
+            var result = CountryService.FilterByCountryName(countries, "U");
+
+            Assert.IsNotNull(result);
+            Assert.That(result.Count(), Is.EqualTo(2));
+            Assert.That(result.Any(x => x.Name.Common == "Ukraine"));
+            Assert.That(result.Any(x => x.Name.Common == "USA"));
+        }
+
+        [Test]
+        public void FilterByCountryName_With_Empty_Filter_Value_Returns_Unfiltered_Records()
+        {
+            var result = CountryService.FilterByCountryName(countries, "");
+
+            Assert.That(result, Is.EqualTo(countries));
+        }
+
+        [Test]
+        public void FilterByPopulation_Returns_Filtered_Records()
+        {
+            var result = CountryService.FilterByPopulation(countries, 30);
+
+            Assert.IsNotNull(result);
+            Assert.That(result.Count(), Is.EqualTo(2));
+            Assert.That(result.Any(x => x.Population == 20000000));
+            Assert.That(result.Any(x => x.Population == 18000000));
+        }
+
+        [Test]
+        public void FilterByPopulation_Filter_With_Less_Than_0_Value_Returns_Unfiltered_Records()
+        {
+            var result = CountryService.FilterByPopulation(countries, -100);
+
+            Assert.That(result, Is.EqualTo(countries));
+        }
+
+        [Test]
+        public void SortByCountryName_Returns_Sorder_In_Descend_Order()
+        {
+            var result = CountryService.SortByCountryName(countries, "descend");
+
+            Assert.That(result.Count, Is.EqualTo(4));
+            Assert.That(result.First().Name.Common, Is.EqualTo("USA"));
+            Assert.That(result.Last().Name.Common, Is.EqualTo("France"));
+        }
+
+        [TestCase("ascend")]
+        [TestCase("")]
+        [TestCase("descend1")]
+        public void SortByCountryName_Returns_Sorder_In_Ascend_Order(string order)
+        {
+            var result = CountryService.SortByCountryName(countries, order);
+
+            Assert.That(result.Count, Is.EqualTo(4));
+            Assert.That(result.First().Name.Common, Is.EqualTo("France"));
+            Assert.That(result.Last().Name.Common, Is.EqualTo("USA"));
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(2, 2)]
+        [TestCase(15, 4)]
+        [TestCase(-1, 4)]
+        public void GetFirstNRecords(int n, int expectedNumber)
+        {
+            var result = CountryService.GetFirstNRecords(countries, n);
+
+            Assert.That(result.Count, Is.EqualTo(expectedNumber));
+        }
+    }
+}
